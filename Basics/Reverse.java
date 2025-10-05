01932dd31e2948b599bad850562c52533780dcbe
Hafeez <syedhafeez957@gmail.com>
2025-10-05 20:55:05 +0530

    Reverse a given number

---
+++ Basics/Reverse.java
@@ -0,0 +1,22 @@
+//Reversing a given number
+
+import java.util.Scanner;
+class Reverse{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number to be reversed");
+        int num=sc.nextInt();
+        System.out.println(reversing(num));
+    }
+
+    public static int reversing(int n){
+        int rev=0;
+        while (n>0) {
+            int lastDigit=n%10;
+            rev=rev*10+lastDigit;
+            n/=10;
+
+        }
+        return rev;
+    }
+}
