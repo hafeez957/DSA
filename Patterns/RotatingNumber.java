5421540887ec1979d1bf1091a41dbad8e8f0607f
Hafeez <syedhafeez957@gmail.com>
2025-10-22 19:04:45 +0530

    Rotating or Cyclic Number Pattern

---
+++ Patterns/RotatingNumber.java
@@ -0,0 +1,16 @@
+
+// Rotating / Cyclic Number Pattern
+
+class RotatingNumber{
+    public static void main(String[] args) {
+        int n=1;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print(" "+(n++)+" ");
+                if(n>9)
+                    n=1;
+            }
+            System.out.println();
+        }
+    }
+}
