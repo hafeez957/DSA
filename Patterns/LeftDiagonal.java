95de402e9dbd84c0d95c13cfb19096784ef4b5ce
Hafeez <syedhafeez957@gmail.com>
2025-10-23 17:48:24 +0530

    Left Diagonal Pattern

---
+++ Patterns/LeftDiagonal.java
@@ -0,0 +1,19 @@
+
+// Left Diagonal Pattern
+
+class LeftDiagonal{
+    public static void main(String[] args) {
+        int n=1;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                if(i==j)
+                    System.out.print(" * ");
+                else
+                    System.out.print("   ");
+                 
+            }
+            System.out.println();
+        }
+    }
+
+}
