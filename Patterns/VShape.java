1d71ff234d26e14143f8c60dd3fa78d160733787
Hafeez <syedhafeez957@gmail.com>
2025-10-24 21:23:26 +0530

    V Shape Pattern

---
+++ Patterns/VShape.java
@@ -0,0 +1,17 @@
+
+// V Shape Pattern
+
+class VShape{
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=9;j++){
+                if(i==j||i+j==10)
+                    System.out.print(" * ");
+                else
+                    System.out.print("   ");
+                 
+            }
+            System.out.println();
+        }
+    }
+}
