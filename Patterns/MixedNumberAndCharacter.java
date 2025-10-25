bdb1795dc8145bdba8b56df62401831fc719ce50
Hafeez <syedhafeez957@gmail.com>
2025-10-25 23:40:49 +0530

    Mixed Number and Character Pattern

---
+++ Patterns/MixedNumberAndCharacter.java
@@ -0,0 +1,18 @@
+
+// Mixed Number and Character Pattern
+
+class MixedNumberAndCharacter{
+    public static void main(String[] args) {
+        int n=1;
+        char c='A';
+        for(int i=1;i<=6;i++){
+            for(int j=1;j<=3;j++){
+                if(i%2!=0)
+                    System.out.print((n++)+" ");
+                else                 
+                    System.out.print((c++)+" ");                 
+            }
+            System.out.println();
+        }
+    }
+}
