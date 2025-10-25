8ffdde868b1b1f96bac148ad2c115fda406d0124
Hafeez <syedhafeez957@gmail.com>
2025-10-25 23:11:08 +0530

    Number Five (5) Shape Pattern

---
+++ Patterns/FiveShape.java
@@ -0,0 +1,17 @@
+
+// Number Five (5) Shape Pattern
+
+class FiveShape{
+     public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                if(i==1||i==3||i==5||i+j==3||i+j==9)
+                    System.out.print(" * ");
+                else
+                    System.out.print("   ");
+                 
+            }
+            System.out.println();
+        }
+    }
+}
