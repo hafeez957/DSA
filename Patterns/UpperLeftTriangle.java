e7a6b434822421a212c02f72de83133403ef5526
Hafeez <syedhafeez957@gmail.com>
2025-10-26 22:15:46 +0530

    Upper Left Triangle Star Pattern

---
+++ Patterns/UpperLeftTriangle.java
@@ -0,0 +1,20 @@
+
+// Upper Left Triangle Star Pattern
+
+class UpperLeftTriangle{
+    public static void main(String[] args) {
+        int star=5;
+        int space=0;
+       for(int r=1;r<=5;r++){
+        for(int i=1;i<=star;i++){
+            System.out.print(" * ");
+        }
+         for(int i=1;i<=space;i++){
+            System.out.print("   ");
+        }
+        System.out.println();
+        star--;
+        space++;
+       }
+    }
+}
