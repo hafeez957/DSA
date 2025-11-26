d7e27a5f93ad3e685e969a2693f4232ddbc72d0b
Hafeez <syedhafeez957@gmail.com>
2025-11-26 22:57:42 +0530

    Finding frenquency of elements in an Array

---
+++ Intermediate/Arrays/FrequencyOfElements.java
@@ -0,0 +1,19 @@
+class FrequencyOfElements{
+    public static void main(String[] args) {
+        int[] arr={1,2,5,3,4,3,2,8,7,0,8,7,1};
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            int cd=1;
+            if(arr[i]!=Integer.MIN_VALUE){
+                for(int j=i+1;j<n;j++){
+                    if(arr[i]==arr[j]){
+                        arr[j]=Integer.MIN_VALUE;
+                        cd++;
+                    }
+                }
+            }
+            if(arr[i]!=Integer.MIN_VALUE)
+                System.out.println(arr[i]+" is present "+cd+" times");
+        }
+    }
+}
