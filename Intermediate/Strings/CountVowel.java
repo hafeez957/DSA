55f35b2ab73f88da560070a4679d676e7684eec2
Hafeez <syedhafeez957@gmail.com>
2025-12-04 22:36:13 +0530

    Counting vowels in a sentence

---
+++ Intermediate/Strings/CountVowel.java
@@ -0,0 +1,12 @@
+class CountVowel{
+    public static void main(String[] args) {
+        String s="KSRTC drivers are next level".toLowerCase();
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                count++;
+            }
+        }
+    }
+}
