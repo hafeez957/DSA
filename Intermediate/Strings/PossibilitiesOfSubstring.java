ed0dd09979a615d0da3a682194efcad8688b0d24
Hafeez <syedhafeez957@gmail.com>
2025-12-08 22:37:58 +0530

    updated

---
+++ Intermediate/Strings/PossibilitiesOfSubstring.java
@@ -0,0 +1,13 @@
+
+// All Possibilities of Substring
+
+class PossibilitiesOfSubstring{
+    public static void main(String[] args) {
+        String s="Avengers";
+        for(int i=0;i<s.length();i++){
+            for(int j=i;j<s.length();j++){
+            System.out.println(s.substring(i,j+1));
+            }
+        }
+    }
+}
